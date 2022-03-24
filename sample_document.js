52c27c1d1178a6d412ce5ecd5d5aaea6ac50b801
michael-gerstenberg <michael@gerstenberg.ch>
2022-03-24 10:13:39 +0100

    init

---
+++ sample_document.js
@@ -0,0 +1,37 @@
+{
+    /** 
+* Paste one or more documents here
+*/
+    "_id": {
+        "$oid": "623c33d5b9fed1ea8180b816"
+    },
+    "schema_version": 1,
+    "rc_identifier": "FLEXIBLE",
+    "valid": {
+        "atlas": true,
+        "ea": true,
+        "community": true
+    },
+    "product_name": "Document Model",
+    "rc_extended": {
+        "de": {
+            "rc_title": "Die Moeglichkeit Documente ohne Downtime ...",
+            "description": "MongoDB kann das mit Document Model ..."
+        },
+        "en": {
+            "rc_title": "The Ability to change the schema without downtime",
+            "description": "MongoDB can do because ..."
+        }
+    },
+    "sources": [
+        {
+            "title": "Document Model",
+            "link": "https://www.mongodb.com/docs/manual/core/data-modeling-introduction/"
+        }, {
+            "title": "Schema Designs",
+            "link": "https://www.mongodb.com/docs/manual/core/data-modeling-introduction/"
+        }
+    ],
+    "comments": "Very strong RC if competitor is RDBMS...",
+    "tags": ["document_db", "cosmos_db"]
+}
